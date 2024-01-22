549073d9930084fe4015e336dd56a2ca0661a6a6
xelA-S <asmith6398@gmail.com>
2024-01-22 09:16:22 +0000

    Added first draft and implemented testing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import "./App.css"
+import Test from './components/Test';
+import { Container, Button, ButtonGroup, Paper,TextField,Stack } from '@mui/material';
+import { ReactComponent as Logo } from "./images/space01.svg"
+export function App() {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    const [order, setOrder] = useState("ascending");
+    const [search, setSearch] = useState("")
+
+
+
+
+    const sortData = (data,sortOrder) => {
+        if (data !== [] ) {
+            if (sortOrder == "ascending") {
+                return data.sort((a,b) => a.price - b.price).slice(0,5)
+            }
+            if (sortOrder == "descending") {
+                return data.sort((a, b) => b.price - a.price).slice(0, 5)
+            }
+        }
+        return data
+
+    }
+    let inputHandler = (e) => {
+        // convert input text to lower case
+        var lowerCase = e.target.value.toLowerCase();
+        setSearch(lowerCase);
+    };
+    const filterData = (data,search) => {
+        return data.filter((item) => {
+            if (search === '') {
+                return data;
+            }
+            else {
+                // return the item which contains the user input
+                return item.name.toLowerCase().includes(search)
+            }
+            
+        })
+    }
+
+    return (
+        <div >
+            <Container class="header">
+                <h1 className="font-weight-light"><Logo class="logo"/> Test Solution</h1>
+            </Container>
+            <Stack
+                direction="row"
+                spacing={2}
+                justifyContent="center"
+                alignItems="flex-end"
+            >
+                <ButtonGroup variant="contained" aria-label="outlined primary button group" class="buttonGroup" >
+                            <Button onClick={() => { setOrder("") } }>View All</Button>
+                            <Button onClick={() => { setOrder("ascending") }}>Ascending</Button>
+                            <Button onClick={() => { setOrder("descending") }}>Descending</Button>
+                        </ButtonGroup>
+                <TextField
+                    id="outlined-basic"
+                    onChange={inputHandler}
+                    variant="filled"
+                    label="Search Items"
+                    color="primary"
+                />
+
+            </Stack>
+            <Container class="center">
+                <Paper class="paper" elevation={3}>
+                    <Test sortData={sortData} order={order} search={search} filterData={filterData} />
+                </Paper>
+            </Container>
+            <Container class="header">
+                <h1 className="font-weight-light"><Logo class="logo" /> Test Solution</h1>
+            </Container>
+            
+        </div>
+        //<div class="center">
+        //    <h1 class="display-inline-block">Hello world</h1>
+            
+        //</div>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,82 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const sortData = (data, sortOrder) => {
+    if (data !== []) {
+        if (sortOrder == "ascending") {
+            return data.sort((a, b) => a.price - b.price).slice(0, 5)
+        }
+        if (sortOrder == "descending") {
+            return data.sort((a, b) => b.price - a.price).slice(0, 5)
+        }
+    }
+    return data
+
+}
+
+let items = [
+    { name: 'Item A', price: -125 },
+    { name: 'Item B', price: -230 },
+    { name: 'Item C', price: -295 },
+    { name: 'Item D', price: -245 },
+    { name: 'Item E', price: -900 },
+    { name: 'Item F', price: -875 },
+    { name: 'Item G', price: -235 },
+    { name: 'Item H', price: -400 },
+]
+//console.log("this is the result", sortData(items, "ascending"))
+
+
+describe('Testing sorts on various price values', () => {
+
+    test("sorting negative values", () => {
+        console.log("this is the result", sortData(items, "descending"))
+
+        expect(sortData(items, "ascending")).toBeInstanceOf(Array);
+        expect(sortData(items, "ascending")).toEqual([
+            { name: 'Item E', price: -900 },
+            { name: 'Item F', price: -875 },
+            { name: 'Item H', price: -400 },
+            { name: 'Item C', price: -295 },
+            { name: 'Item D', price: -245 },
+        ])
+        expect(sortData(items, "descending")).toEqual([
+            { name: 'Item A', price: -125 },
+            { name: 'Item B', price: -230 },
+            { name: 'Item G', price: -235 },
+            { name: 'Item D', price: -245 },
+            { name: 'Item C', price: -295 },
+        ])
+
+
+
+    })
+    test("sorting repeat values", () => {
+        console.log("this is the result", sortData(items, "ascending"))
+        items[0].price = items[1].price;
+        items[2].price = items[3].price;
+
+        expect(sortData(items, "ascending")).toBeInstanceOf(Array);
+        expect(sortData(items, "ascending")).toEqual([
+            { name: 'Item E', price: -875 },
+            { name: 'Item F', price: -875 },
+            { name: 'Item H', price: -295 },
+            { name: 'Item C', price: -295 },
+            { name: 'Item D', price: -245 },
+        ])
+        expect(sortData(items, "descending")).toEqual([
+            { name: 'Item A', price: -125 },
+            { name: 'Item B', price: -230 },
+            { name: 'Item G', price: -235 },
+            { name: 'Item D', price: -245 },
+            { name: 'Item H', price: -295 },
+        ])
+
+
+
+    })
+    test("sorting zero values", () => {
+       
+
+    })
+
+
 });
